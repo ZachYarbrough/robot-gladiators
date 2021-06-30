04c01327862bcd3988de3259728bba16ce7fc4fb
Zach Yarbrough <zachyarbro@gmail.com>
2021-06-30 10:32:51 -0500

    add robot variables and alerts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,41 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+if(playerHealth > 0) {
+    console.log("Your player is still alive!");
+}
+
+if(playerHealth == 0) {
+    console.log("This will not run");
+} 
+else {
+    console.log("This will run instead");
+}
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    if(playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } 
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+};
+
+fight();
